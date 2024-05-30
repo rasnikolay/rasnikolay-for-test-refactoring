7fa8cacea4694b205e3aa9906bfa67b315645528
Krivega <mr.krivega@gmail.com>
2024-05-30 12:57:29 +0300

    Update

--- src/LimitedStack.ts
+++ src/LimitedStack.ts
@@ -1,24 +1,11 @@
 interface Stack<T> {
   pop(): T | null;
   push(item: T): void;
-}
-
-class EmptyException extends Error {}
-class FullException extends Error {}
-
-interface BoundedStack<T> extends Stack<T> {
   percentFull(): number;
 }
 
-class LimitedStack<T> implements BoundedStack<T> {
-  pop(): T | null {
-    throw new Error("Method not implemented.");
-  }
-  push(item: T): void {
-    throw new Error("Method not implemented.");
-  }
+class UnlimitedStack<T> implements Stack<T> {
   percentFull(): number {
-    // Конкретная реализация для ограниченного стека
-    return 0
+    return 0;
   }
 }
